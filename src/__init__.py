75d14a150ef0fcf84c96a3e0bbef482b855ea990
lishengaiuse-hub <lisheng.aiuse@gmail.com>
2026-04-04 15:39:05 +0800

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Samsung CE Intelligence System
+Enterprise-grade news aggregation with multi-layer deduplication
+"""
+
+__version__ = "2.0.0"
+__author__ = "Samsung CE Intelligence Team"
